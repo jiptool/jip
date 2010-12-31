f735b4d84916a5593f29f74de7042587b9658b74
Sun Ning <classicning@gmail.com>
2010-12-31 17:17:41 +0800

    setup scripts

---
+++ setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/jython
+# Copyright (C) 2011 Sun Ning<classicning@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from distutils.core import setup
+
+setup(
+    name="jip",
+    version="0.1dev",
+    author="Sun Ning",
+    author_email="classicning@gmail.com",
+    url="https://github.com/sunng87/jip",
+    description="jip install packages, for Jython",
+    scripts = ["jip"],
+    license='gpl',
+    py_modules=['jip'],
+    classifiers=['Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPL',
+        'Topic :: Software Development',
+        'Programming Language :: Jython :: 2.5',
+        'Programming Language :: Java',
+        'Environment :: Console',
+        'Operating System :: POSIX']
+)
+
+
