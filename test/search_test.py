a015ada96645eef22d2e941e7945ff5753df0c50
baztian <bastian.dev@gmail.com>
2015-04-22 21:22:30 +0200

    Make tests pass.

--- test/search_test.py
+++ test/search_test.py
@@ -9,23 +9,23 @@ class SonatypeMavenSearchTests(unittest.TestCase):
     def testGenericSearch(self):
         query = "commons-lang"
         results =self.searcher.search(query)
-        self.assert_(results is not None)
-        self.assert_(len(results) > 0)
-        self.assert_(results[0][0] is not None)
-        self.assert_(results[0][1] is not None)
-        self.assert_(results[0][2] is not None)
-        self.assert_(results[0][3] is not None)
+        self.assertTrue(results is not None)
+        self.assertTrue(len(results) > 0)
+        self.assertTrue(results[0][0] is not None)
+        self.assertTrue(results[0][1] is not None)
+        self.assertTrue(results[0][2] is not None)
+        self.assertTrue(results[0][3] is not None)
 
     def testGroupAndArtifactSearch(self):
         group = "commons-lang"
         artif = "commons-lang"
         results =self.searcher.search_group_artifact(group, artif)
-        self.assert_(results is not None)
-        self.assert_(len(results) > 0)
+        self.assertTrue(results is not None)
+        self.assertTrue(len(results) > 0)
         self.assertEquals(results[0][0], group)
         self.assertEquals(results[0][1], group)
-        self.assert_(results[0][2] is not None)
-        self.assert_(results[0][3] is not None)
+        self.assertTrue(results[0][2] is not None)
+        self.assertTrue(results[0][3] is not None)
 
 if __name__ == '__main__':
     unittest.main()
