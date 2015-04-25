3cf2936fc32897b7785493d65d09e655cb94c235
baztian <bastian.dev@gmail.com>
2015-04-25 17:21:01 +0200

    Install unittest2 to make skipUnless work for Python 2.6.

--- test/emdebtest.py
+++ test/emdebtest.py
@@ -1,5 +1,8 @@
 import sys
-import unittest
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
 
 from jip.embed import require
 
@@ -7,9 +10,8 @@ def is_jython():
     return sys.platform.lower().startswith('java')
 
 class JipRequireTests(unittest.TestCase):
+    @unittest.skipUnless(is_jython(), "requires jython")
     def testRequire(self):
-        if is_jython():
-            return
         require("commons-lang:commons-lang:2.6")
         from org.apache.commons.lang import StringUtils
 
