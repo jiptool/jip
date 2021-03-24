73d38d0f8c0f13070b62c8b42d8c024d59c8287f
Mohamed Gad-Elrab <gmo7rng@bosch.com>
2021-03-24 21:45:12 +0100

    Original code do not handle the BOM, or the Byte Order Mark (http://en.wikipedia.org/wiki/Byte_order_mark) causing parsing errors

--- jip/maven.py
+++ jip/maven.py
@@ -110,7 +110,7 @@ class Pom(object):
             pom_string = re.sub(r"<project(.|\s)*?>", '<project>', self.pom_string, 1)
             parser = ElementTree.XMLParser(target=WhitespaceNormalizer())
             # Remove BOM, or a Byte Order Mark.  http://en.wikipedia.org/wiki/Byte_order_mark
-            if pom_string.startswith(codecs.BOM_UTF8):
+            if pom_string.startswith("ï»¿"):
                 pom_string = pom_string.replace("ï»¿", "")
             parser.feed(pom_string.encode('utf-8'))
             self.eletree = parser.close()
